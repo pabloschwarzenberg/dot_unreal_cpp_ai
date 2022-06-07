cd6d54942e54bec7a05fbf0f97ec95713a816cb4
Pablo Schwarzenberg <psr@Maia.local>
2022-06-07 11:52:47 -0400

    HUD

--- Source/dot/DotHUD.cpp
+++ Source/dot/DotHUD.cpp
@@ -7,13 +7,4 @@
 void ADotHUD::DrawHUD()
 {
     Super::DrawHUD();
-    if(hudFont)
-    {
-        ADotCharacter* dot = Cast<ADotCharacter>(GetOwningPawn());
-        if(dot)
-        {
-            DrawText(FString::FromInt(dot->frutos),FColor::Black,200,120,hudFont,3,false);
-            DrawText(FString::FromInt(dot->flores),FColor::Black,450,120,hudFont,3,false);
-        }
-    }
 }
